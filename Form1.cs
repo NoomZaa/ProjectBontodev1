94c43a58f35a9474d4a3ac402409ad8aba6a669a
NoomZaa <noomstudio4@gmail.com>
2022-09-18 21:54:05 +0700

    Login OK

--- Form1.cs
+++ Form1.cs
@@ -13,20 +13,18 @@ namespace ProjectBontodev1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
-
+        string strCon = "Data Source=NOOMZA;Initial Catalog=Project1Bontodev; Integrated Security=True";
         SqlConnection conObj = new SqlConnection();
         SqlDataAdapter da;
-        DataSet ds = new DataSet();
+        //DataSet ds = new DataSet();
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string strCon;
-
-            strCon = "Data Source=NOOMZA;Initial Catalog=Project1Bontodev; Integrated Security=True";
 
             if(conObj.State == ConnectionState.Open)
             {
@@ -38,15 +36,69 @@ namespace ProjectBontodev1
                 conObj.Open();
             }
 
-            string strSQL = "select * from CustomerID;";
 
+            //string strSQLShow = "select * from tb_CustomerID";
+            //da = new SqlDataAdapter(strSQLShow, conObj);
+
+            //ds.Tables.Clear();
+            //da.Fill(ds, "MyQuery");
+            //dataGridView1.DataSource = ds.Tables["MyQuery"];
+
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            string strSQL = "select * from tb_CutomerLogin where Username = '" + txtUser.Text.Trim() + "' and Password = '"+txtPassword.Text.Trim()+"'";
             da = new SqlDataAdapter(strSQL, conObj);
+            DataTable dtbl = new DataTable();
+            da.Fill(dtbl);
 
-            ds.Tables.Clear();
-            da.Fill(ds, "MyQuery");
-            dataGridView1.DataSource = ds.Tables["MyQuery"];
+            if(dtbl.Rows.Count > 0)
+            {
+                main showmain = new main();
+                this.Hide();
+                showmain.Show();
+            }
+            else if (txtUser.Text == "")
+            {
+                MessageBox.Show("Enter The Username", "Login");
+                txtUser.Text = "";
+                txtPassword.Text = "";
+                txtUser.Focus();
+            }
+            else if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Enter The Password", "Login");
+                txtUser.Text = "";
+                txtPassword.Text = "";
+                txtUser.Focus();
+            }
+            else
+            {
+                MessageBox.Show("กรุณาติดต่อผู้พัฒนาโปรแกรม", "Login",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
 
 
+            //string username, password;
+            //username = txtUser.Text;
+            //password = txtPassword.Text;
+
+            //string strSQL = "select * from CustomerID where Username = '" + txtUser.Text + "' and '" + txtPassword.Text + "' ";
+            //SqlDataAdapter qu = new SqlDataAdapter(strSQL,strCon);
+            //DataTable dt = new DataTable();
+            //qu.Fill(dt);
+
+            //if(dt.Rows.Count>0)
+            //{
+            //    MessageBox.Show("เข้าสู่ระบบเรียบร้อย", "Login");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("กรุณาตรวจสอบ User และ Password", "Login");
+            //}
+            
         }
     }
 }
